be1cd2a4043f87d073446f4922e24aecf0835e9b
stoichitescumaria <stoichitescumaria@yahoo.com>
2023-04-06 18:18:07 +0300

    Initial Commit

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var08/PracticalTest01Var08MainActivity.java
@@ -0,0 +1,14 @@
+package ro.pub.cs.systems.eim.practicaltest01var08;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PracticalTest01Var08MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test01_var08_main);
+    }
+}
